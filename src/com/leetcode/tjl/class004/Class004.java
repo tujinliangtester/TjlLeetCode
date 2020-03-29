9ac4c8c2c2285d91a81709ff63f0287f1ebc29d6
tujinliangtester <tujinliangtester@163.com>
2020-03-29 19:04:31 +0800

    做了一些调整，但还是非常乱，是不是需要一个清晰的逻辑，从头再来？

--- src/com/leetcode/tjl/class004/Class004.java
+++ src/com/leetcode/tjl/class004/Class004.java
@@ -4,26 +4,15 @@ import java.util.Arrays;
 
 public class Class004 {
     static float equalNum = (float) 0.1;
+    static final String S2S = "single2single";
+    static final String S2D = "single2double";
+    static final String D2S = "double2single";
+    static final String D2D = "double2double";
 
     public static void main(String[] args) {
-        int[] a = {1, 3};
-        int[] b = {2,4};
+        int[] a = {3};
+        int[] b = {2};
         System.out.println(myMain(a, b));
-    }
-
-    static int[] getMidIndex(int[] m) {
-        int len = m.length;
-        int[] res = new int[2];
-        if (len == 0) {
-            return res;
-        }
-        if (len % 2 == 0) {
-            res[0] = len / 2 - 1;
-            res[1] = len / 2;
-        } else {
-            res[0] = len / 2;
-        }
-        return res;
     }
 
 
@@ -45,6 +34,27 @@ public class Class004 {
         float midValue;
         midValueA = getMidValue(a, midIndexA);
         midValueB = getMidValue(b, midIndexB);
+
+
+        //首先用中位数来判断，然后在需要具体的index或者长度的时候，暂时这样处理，对于有两个中位索引的：
+        //中位数小的：取index[0]；  中位数大的：取index[1]
+        int tmpMidIndexA;
+        int tmpMidIndexB;
+        String jt = judgeType(a, b);
+        if (S2S.equals(jt)) {
+            tmpMidIndexA = midIndexA[0];
+            tmpMidIndexB = midIndexB[0];
+        } else if (S2D.equals(jt)) {
+            tmpMidIndexA = midIndexA[0];
+            tmpMidIndexB = midIndexB[1];
+        } else if (D2S.equals(jt)) {
+            tmpMidIndexA = midIndexA[0];
+            tmpMidIndexB = midIndexB[0];
+        } else {
+            tmpMidIndexA = midIndexA[0];
+            tmpMidIndexB = midIndexB[1];
+        }
+
         if (midValueA - midValueB < equalNum && midValueA - midValueB > -equalNum) {
             midValue = midValueA;
         } else if (midValueA - midValueB > equalNum) {
@@ -52,18 +62,22 @@ public class Class004 {
             //大值中位，等于，小值数组的最大值
             if (midValueA - b[b.length - 1] < equalNum && midValueA - b[b.length - 1] > -equalNum) {
                 k = b.length - 1;
-                // 长度足够
-                //if (midIndexA[0] >= midIndexB[0]) {
-                midValue = myMain(Arrays.copyOfRange(a, midIndexA[0] - (k - midIndexB[0]), midIndexA[0] + 1),
-                        Arrays.copyOfRange(b, midIndexB[0], k + 1)
-                );
-                // }
-                // 长度不够 todo
+
+                int[] copyedA = judgeAndCopy(a, tmpMidIndexA - (k - tmpMidIndexB), tmpMidIndexA + 1);
+                int[] copyedB = judgeAndCopy(b, tmpMidIndexB, k + 1);
+                if (copyedA == null || copyedA.length == 0) {
+                    return getMidValueByArray(copyedB);
+                }
+                if (copyedB == null || copyedB.length == 0) {
+                    return getMidValueByArray(copyedA);
+                }
+                return myMain(copyedA, copyedB);
             }
             //大值中位，大于，小值数组的最大值 参考图思路细化2
+            //todo 感觉还是乱，不论是索引的起，还是止；还是递归的逻辑，都感觉非常非常的乱！ 后续跟进
             else if (midValueA - b[b.length - 1] > equalNum) {
 
-                k = getCloseIndex(b[b.length - 1], Arrays.copyOfRange(a, 0, midIndexA[0] + 1));
+                k = getCloseIndex(b[b.length - 1], Arrays.copyOfRange(a, 0, midIndexA[0] + 1),tmpMidIndexB);
                 if (k == -1) {
                     // todo
                     // 做的时候感觉还是太复杂、混淆了，当然也可能这么做本身是对的，只是还需要再理一理
@@ -75,7 +89,7 @@ public class Class004 {
                 else if (k == 0) {
                     //这里暂时和下面的 w<0 中逻辑一样，调试了再说
                     int tmpIndex = (a.length - b.length) / 2 - 1;
-                    tmpIndex = tmpIndex >= 0 ? tmpIndex:0;
+                    tmpIndex = tmpIndex >= 0 ? tmpIndex : 0;
                     return a[tmpIndex];
                 }
                 //这里的y,w是指长度
@@ -89,9 +103,9 @@ public class Class004 {
                 midValue = myMain(Arrays.copyOfRange(a, midIndexA[0] - (y + w), midIndexA[0] - y + 1),
                         Arrays.copyOfRange(b, b.length - 1 - w, b.length));
             }
-            //大值中位，小于，小值数组的最大值 参考图思路细化1
+            //大值中位，小于，小值数组的最大值 参考图思路细化1 todo
             else {
-                k = getCloseIndex(midValueA, Arrays.copyOfRange(b, midIndexB[0], b.length));
+                k = getCloseIndex(midValueA, Arrays.copyOfRange(b, tmpMidIndexB, b.length),tmpMidIndexB);
                 if (k == -1) {
 //                    todo
                     //这里暂时和下面的 w<0 中逻辑一样，调试了再说
@@ -114,6 +128,21 @@ public class Class004 {
         return midValue;
     }
 
+    static int[] getMidIndex(int[] m) {
+        int len = m.length;
+        int[] res = new int[2];
+        if (len == 0) {
+            return res;
+        }
+        if (len % 2 == 0) {
+            res[0] = len / 2 - 1;
+            res[1] = len / 2;
+        } else {
+            res[0] = len / 2;
+        }
+        return res;
+    }
+
     //获取数组m，给定中位id时对应的值
     static float getMidValue(int[] m, int[] midIndex) {
         float midValue;
@@ -126,51 +155,79 @@ public class Class004 {
         return midValue;
     }
 
+    //获取数组m，直接获取中位值
+    static float getMidValueByArray(int[] m) {
+        int[] midIndex = getMidIndex(m);
+        return getMidValue(m, midIndex);
+    }
+
+
+    /*
     //获取m中，与v最接近的值对应的索引
     // （等于v，或者比v大的最小值索引，这里如果取小于的话，对后面的组合起来的处理可能会有问题）
-    static int getCloseIndex(float v, int[] m) {
+    //注意，与前诉逻辑一直，在中位数较小的数组中，找索引，都尽量返回较大的那个，如midIndex[1]
+    * @param v:目标值
+     * @param m:目标数组
+     * @param exIndex:在返回的索引基础上，额外增加的索引值
+     @return
+    * */
+    static int getCloseIndex(float v, int[] m, int exIndex) {
         int index;
         if (m.length == 0) {
             index = -1;
-            return index;
+            return index+exIndex;
         }
-        if (v - m[m.length / 2] < equalNum && v - m[m.length / 2] > -equalNum) {
-            index = m.length / 2;
-        } else if (v - m[m.length / 2] > equalNum) {
+        float tmpMidValue = getMidValueByArray(m);
+        int[] tmpMidIndex = getMidIndex(m);
+
+        if (v - tmpMidValue < equalNum && v - tmpMidValue > -equalNum) {
+            //注意，与前诉逻辑一直，在中位数较小的数组中，找索引，都尽量返回较大的那个
+            return Math.max(tmpMidIndex[1], tmpMidIndex[0])+exIndex;
+        } else if (v - tmpMidValue > equalNum) {
             if (m.length == 1) {
-                if (v > m[0]) {
-                    index = -1;
-                    return index;
-                }
-                index = 0;
-                return index;
+                index = -1;
+                return index+exIndex;
             }
-            index = getCloseIndex(v, Arrays.copyOfRange(m, m.length / 2 + 1, m.length));
-            if (index == -1) {
-                index = m.length / 2;
+            index = getCloseIndex(v, Arrays.copyOfRange(m, Math.max(tmpMidIndex[1], tmpMidIndex[0]), m.length),Math.max(tmpMidIndex[1], tmpMidIndex[0])-1);
+            if (index <Math.max(tmpMidIndex[1], tmpMidIndex[0])-1) {
+                return  m.length / 2+exIndex;
             }
         } else {
             if (m.length == 1) {
                 if (v > m[0]) {
                     index = -1;
-                    return index;
+                    return index+exIndex;
                 }
                 index = 0;
-                return index;
+                return index+exIndex;
             }
-            index = getCloseIndex(v, Arrays.copyOfRange(m, 0, m.length / 2));
-            if (index == -1) {
-                index = m.length / 2;
+            index = getCloseIndex(v, Arrays.copyOfRange(m, 0, m.length / 2),Math.max(tmpMidIndex[1], tmpMidIndex[0]));
+            if (index <Math.max(tmpMidIndex[1], tmpMidIndex[0])) {
+                return  m.length / 2+exIndex;
             }
         }
-        return index;
+        return index+exIndex;
     }
 
-    static int[] judgeAndCopy(int[] m,int i,int j){
-        if (m.length==0){
-            //todo
+    static int[] judgeAndCopy(int[] m, int i, int j) {
+        if (m.length == 0 || i < 0 || i >= j || j > m.length) {
+            return null;
         }
-        //todo
-        return null;
+        return Arrays.copyOfRange(m, i, j);
+    }
+
+    static String judgeType(int[] m, int[] n) {
+        int[] midIndexM = getMidIndex(m);
+        int[] midIndexN = getMidIndex(n);
+        if (midIndexM[1] > midIndexM[0]) {
+            if (midIndexN[1] > midIndexN[0]) {
+                return D2D;
+            }
+            return D2S;
+        }
+        if (midIndexN[1] > midIndexN[0]) {
+            return S2D;
+        }
+        return S2S;
     }
 }
